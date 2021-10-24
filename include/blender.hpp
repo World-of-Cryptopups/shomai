dea3c90647c265c524529ee19d9046e4673206ad
TheBoringDude <iamcoderx@gmail.com>
2021-10-24 23:45:06 +0800

    base functions

--- include/blender.hpp
+++ include/blender.hpp
@@ -1,20 +1,87 @@
 #include <eosio/eosio.hpp>
+#include <eosio/singleton.hpp>
+#include "atomicassets.hpp"
+#include "atomicdata.hpp"
 
 using namespace std;
 using namespace eosio;
 
-CONTRACT blender : public contract {
-  public:
-    using contract::contract;
+CONTRACT blender : public contract
+{
+public:
+  using contract::contract;
 
-    ACTION hi(name from, string message);
-    ACTION clear();
+  /*
+    Start Blend Actions
+    */
+  ACTION makeburner(name user, name targetcol, name targettemp, vector<uint64_t> ingredients);
+  ACTION makeswapper(name user, name targetcol, name targettemp, vector<uint64_t> ingredients);
+  ACTION delburner(name user, uint64_t burnerid);
+  ACTION delswapper(name user, uint64_t swapperid);
+  ACTION blendswapper(name blender, vector<uint64_t> assetids);
+  ACTION blendburner(name blender, vector<uint64_t> assetids);
+  /*
+    End Blend Actions
+    */
 
-  private:
-    TABLE messages {
-      name    user;
-      string  text;
-      auto primary_key() const { return user.value; }
-    };
-    typedef multi_index<name("messages"), messages> messages_table;
+private:
+  TABLE burner_s
+  {
+    uint64_t burnerid;
+    name user;
+    name targetcol;
+    name targettemp;
+    vector<uint64_t> ingredients;
+
+    uint64_t primary_key() const { return burnerid; };
+    uint64_t by_user() const { return user.value; };
+  };
+
+  TABLE swapper_s
+  {
+    uint64_t swapperid;
+    name user;
+    name targetcol;
+    name targettemp;
+    vector<uint64_t> ingredients;
+
+    uint64_t primary_key() const { return swapperid; };
+    uint64_t by_user() const { return user.value; };
+  };
+
+  TABLE config_s
+  {
+    uint64_t burnercounter = 100000;
+    uint64_t swappercounter = 100000;
+  };
+
+  typedef singleton<"config"_n, config_s> config_t;
+  typedef multi_index<"config"_n, config_s> config_t_for_abi;
+  typedef multi_index<"burners"_n, burner_s, indexed_by<"user"_n, const_mem_fun<burner_s, uint64_t, &burner_s::by_user>>> burner_t;
+  typedef multi_index<"swappers"_n, swapper_s, indexed_by<"user"_n, const_mem_fun<swapper_s, uint64_t, &swapper_s::by_user>>> swapper_t;
+
+  /* Initialize tables */
+  config_t config = config_t(_self, _self.value);
+  burner_t burner = burner_t(_self, _self.value);
+  swapper_t swapper = swapper_t(_self, _self.value);
+
+  // ======== util functions
+
+  /*
+      Check if user is authorized to mint NFTs
+   */
+  bool isAuthorized(name collection, name user)
+  {
+    auto itrCollection = atomicassets::collections.require_find(collection.value, "No collection with this name exists!");
+    bool authorized = false;
+    vector<name> authAccounts = itrCollection->authorized_accounts;
+    for (auto it = authAccounts.begin(); it != authAccounts.end() && !authorized; it++)
+    {
+      if (user == name(*it))
+      {
+        authorized = true;
+      }
+    }
+    return authorized;
+  }
 };
--- src/blender.cpp
+++ src/blender.cpp
@@ -1,37 +1,61 @@
 #include <blender.hpp>
 
-ACTION blender::hi(name from, string message) {
-  require_auth(from);
-
-  // Init the _message table
-  messages_table _messages(get_self(), get_self().value);
-
-  // Find the record from _messages table
-  auto msg_itr = _messages.find(from.value);
-  if (msg_itr == _messages.end()) {
-    // Create a message record if it does not exist
-    _messages.emplace(from, [&](auto& msg) {
-      msg.user = from;
-      msg.text = message;
-    });
-  } else {
-    // Modify a message record if it exists
-    _messages.modify(msg_itr, from, [&](auto& msg) {
-      msg.text = message;
-    });
-  }
+ACTION blender::makeburner(name user, name targetcol, name targettemp, vector<uint64_t> ingredients)
+{
+  require_auth(user);
 }
 
-ACTION blender::clear() {
-  require_auth(get_self());
+ACTION blender::delburner(name user, uint64_t burnerid)
+{
+  require_auth(user);
 
-  messages_table _messages(get_self(), get_self().value);
+  auto itr = burner.find(burnerid);
 
-  // Delete all records in _messages table
-  auto msg_itr = _messages.begin();
-  while (msg_itr != _messages.end()) {
-    msg_itr = _messages.erase(msg_itr);
-  }
+  // check if burnerid exists
+  check(itr != burner.end(), "Burner ID does not exist!");
+
+  // check if burnerid author/user is user
+  check(itr->user == user, "Burner ID does is not owned by user!");
+
+  // remove item
+  burner.erase(itr);
 }
 
-EOSIO_DISPATCH(blender, (hi)(clear))
+ACTION blender::makeswapper(name user, name targetcol, name targettemp, vector<uint64_t> ingredients)
+{
+  require_auth(user);
+}
+
+ACTION blender::delswapper(name user, uint64_t swapperid)
+{
+  require_auth(user);
+
+  auto itr = swapper.find(swapperid);
+
+  // check if swapperid exists
+  check(itr != swapper.end(), "Swapper ID does not exist!");
+
+  // check if swapperid author/user is user
+  check(itr->user == user, "Swapper ID does is not owned by user!");
+
+  // remove item
+  swapper.erase(itr);
+}
+
+ACTION blender::blendburner(name blender, vector<uint64_t> assetids)
+{
+  require_auth(blender);
+
+  // do not allow the smart contract to blend for itself
+  check(blender != _self, "The smart contract itself should not be able to blend!");
+}
+
+ACTION blender::blendswapper(name blender, vector<uint64_t> assetids)
+{
+  require_auth(blender);
+
+  // do not allow the smart contract to blend for itself
+  check(blender != _self, "The smart contract itself should not be able to blend!");
+}
+
+EOSIO_DISPATCH(blender, (makeburner)(delburner)(makeswapper)(delswapper)(blendburner)(blendswapper))
